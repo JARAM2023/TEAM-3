a12589889f14c20ca204743495572089bf255947
jeno0228 <113761913+jeno0228@users.noreply.github.com>
2023-07-26 20:59:18 +0900

    [FEAT] POST food/search function

--- app/models/user.py
+++ app/models/user.py
@@ -16,17 +16,6 @@ class User(ModelBase):
     gender = Column(sqltypes.Integer, nullable=False)
     
 
-class Food(ModelBase):
-    __tablename__="food"
-    id = Column(sqltypes.Integer, primary_key=True)
-    name = Column(sqltypes.Integer, nullable=False)
-    carbo = Column(sqltypes.Float, nullable=False)
-    protein = Column(sqltypes.Float, nullable=False)
-    fat = Column(sqltypes.Float, nullable=False)
-    sodium = Column(sqltypes.Float, nullable=False)
-    sugars = Column(sqltypes.Float, nullable=False)
-    coles = Column(sqltypes.Float, nullable=False)
-    transfat = Column(sqltypes.Float, nullable=False)
 
 
 
